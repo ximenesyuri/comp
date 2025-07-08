4b9f39dc7ef4020bc825d83d941b8de3ee04149d
yxm <yxm.dev@gmx.ie>
2025-07-08 14:44:46 -0300

    fix:  minor fixing

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,2 @@
 from app.main import *
-from app.components import div, button
-from app.components.buttons import button_close
-from app.components.icons.line import icon_search
-from app.service import preview, render, mock
 
-component = COMPONENT(
-    definer=div * (button * icon_search),
-    context={}
-)
-
-print(render(component))
