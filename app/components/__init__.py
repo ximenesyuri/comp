8a5ac9997fdc1d2134e4e1f045e8889115649596
yxm <yxm.dev@gmx.ie>
2025-07-10 19:23:33 -0300

    fix:  minor fixing

--- app/components/__init__.py
+++ app/components/__init__.py
@@ -7,13 +7,13 @@ from app.nill import nill_jinja
 def div(div: Div=null(Div), alpine: Alpine=null(Alpine)) -> Tag('div'):
     return """jinja
 <div
-    {%if div['div_id'] %}id="{{ div['div_id'] }}"{%endif %}
-    {%if div['div_class']%}class="{{div['div_class']}}"{%endif%}
-    {%if div['div_hover']%}hover="{{div['div_hover']}}"{%endif%}
-    {%if alpine['x_init']%}x-init="{{alpine['x_init']}}"{%endif%}
-    {%if alpine['x_show']%}x-show="{{alpine['x_show']}}"{%endif%}
-    {%if alpine['x_data']%}x-data="{{alpine['x_data']}}"{%endif%}
-    {%if alpine['x_cloak']%}x-cloak{%endif%}
+    {%if div.div_id %}id="{{ div.div_id }}"{%endif %}
+    {%if div.div_clas%}class="{{div.div_class}}"{%endif%}
+    {%if div.div_hover%}hover="{{div.div_hover}}"{%endif%}
+    {%if alpine.x_init%}x-init="{{alpine.x_init}}"{%endif%}
+    {%if alpine.x_show%}x-show="{{alpine.x_show}}"{%endif%}
+    {%if alpine.x_data%}x-data="{{alpine.x_data}}"{%endif%}
+    {%if alpine.x_cloak%}x-cloak{%endif%}
 >
     {{div_inner}}
 </div>
@@ -23,12 +23,11 @@ def div(div: Div=null(Div), alpine: Alpine=null(Alpine)) -> Tag('div'):
 def button(button: Button=null(Button)) -> Tag('button'):
     return """jinja
 <button 
-    {%if button['button_id']%}id="{{button['button_id']}}"{%endif%}
-    {%if button['button_class']%}class="{{button['button_class']}}"{%endif%}
-    {%if button['button_hover']%}hover="{{button['button_hover']}}"{%endif%}
-    type="button"
-    {%if button['on_click']%}@click="{{button['on_click']}}"{%endif%}
-    {%if button['click_away']%}@click_away="{{button['click_away']}}"{%endif%}
+    {%if button.button_id%}id="{{button.button_id}}"{%endif%}
+    {%if button.button_class%}class="{{button.button_class}}"{%endif%}
+    {%if button.button_hover%}hover="{{button.button_hover}}"{%endif%}
+    {%if button.on_click%}@click="{{button.on_click}}"{%endif%}
+    {%if button.click_away%}@click_away="{{button.click_away}}"{%endif%}
 >       
     {{button_inner}}
 </button>
