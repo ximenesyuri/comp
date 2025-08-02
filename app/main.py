279e1b5e63b8efd2c290b8ab5729eb132d4cf8a6
yxm <yxm.dev@gmx.ie>
2025-08-02 13:08:21 -0300

    ...

--- app/main.py
+++ app/main.py
@@ -5,12 +5,10 @@ from app.mods.types.base import *
 from app.mods.types.tag  import *
 from app.mods.factories.base import *
 from app.mods.functions import *
-#from app.mods.service import *
+from app.mods.service import *
 
 @model
 class _SCRIPTS:
     alpine: Optional(PathUrl, "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js")
     htmx: Optional(PathUrl, "https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js")
     flexsearch: Optional(PathUrl, "https://unpkg.com/flexsearch@0.8.2/dist/module/index.js")
-
-SCRIPTS = _SCRIPTS()
