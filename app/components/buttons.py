d9c091568ea788d2404afdda87cca4651eaa3781
yxm <yxm.dev@gmx.ie>
2025-08-07 18:08:14 -0300

    fix:  change the return type of predefined buttons

--- app/components/buttons.py
+++ app/components/buttons.py
@@ -1,4 +1,4 @@
-from app.mods.factories.base import Tag
+from app.mods.types.base import Jinja
 from app.mods.decorators.base import component
 from app.components import button as _button
 from app.models import Button, Icon
@@ -11,21 +11,21 @@ from app.components.icons.line import (
 )
 
 @component
-def button_close(button: Button=Button(), icon: Icon=Icon()) -> Tag('button'):
+def button_close(button: Button=Button(), icon: Icon=Icon()) -> Jinja:
     return (_button * icon_close)(button=button, icon=icon)
 
 @component
-def button_menu(button: Button=Button(), icon: Icon=Icon()) -> Tag('button'):
+def button_menu(button: Button=Button(), icon: Icon=Icon()) -> Jinja:
     return (_button * icon_menu)(button=button, icon=icon)
 
 @component
-def button_search(button: Button=Button(), icon: Icon=Icon()) -> Tag('button'):
+def button_search(button: Button=Button(), icon: Icon=Icon()) -> Jinja:
     return (_button * icon_search)(button=button, icon=icon)
 
 @component
-def button_home(button: Button=Button(), icon: Icon=Icon()) -> Tag('button'):
+def button_home(button: Button=Button(), icon: Icon=Icon()) -> Jinja:
     return (_button * icon_home)(button=button, icon=icon)
 
 @component
-def button_theme_switcher(button: Button=Button(), icon: Icon=Icon()) -> Tag('button'):
+def button_theme_switcher(button: Button=Button(), icon: Icon=Icon()) -> Jinja:
     return (_button * icon_theme_switcher)(button=button, icon=icon)
