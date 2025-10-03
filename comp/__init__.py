e2934221f0f9c72cc931f159b32798e5bbea73b0
yxm <yxm.dev@gmx.ie>
2025-10-03 16:53:30 -0300

    fix: minor fixing

--- comp/__init__.py
+++ comp/__init__.py
@@ -1,3 +1,6 @@
-from comp.main   import *
 from comp.models import *
 from comp.comps  import *
+from comp.decorators import *
+from comp.operations import *
+from comp.service import *
+from comp.types import *
