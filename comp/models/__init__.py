be64633b0527d06272418906c6212b27a647e2cb
yxm <yxm.dev@gmx.ie>
2025-10-23 11:28:37 -0300

    fix: minor fixing

--- comp/models/__init__.py
+++ comp/models/__init__.py
@@ -3,6 +3,6 @@ from comp.models.content    import Button, Icon, Image, Img, Text, Title, Link, 
 from comp.models.extensions import Alpine, HTMX, Search
 from comp.models.form       import Input, Form
 from comp.models.includes   import Script, Asset
-from comp.models.lists      import Item, Unordered, Ul, Ordered, Ol, NavItem, Nav
+from comp.models.lists      import Item, Unordered, Ul, Ordered, Ol, NavItem, CustomNav, Nav
 from comp.models.responsive import Desktop, Tablet, Phone
 from comp.models.structure  import Div, Flex, Inline, Block, Header, Footer, Aside, Sidebar, Column, Col, Row, Grid
